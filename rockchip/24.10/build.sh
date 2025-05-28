320981b504d330868f34fae625c21c39cf28fb43
PrestonHee <44938101+PrestonHee@users.noreply.github.com>
2025-05-28 12:14:10 +0800

    添加zram软件包

--- rockchip/24.10/build.sh
+++ rockchip/24.10/build.sh
@@ -27,6 +27,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES bash"
+PACKAGES="$PACKAGES zram-swap"
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES vim-full"
 PACKAGES="$PACKAGES vim-runtime"
